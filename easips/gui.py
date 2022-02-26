38021c2562d7471442c6fd40540517fc1f1ffa31
Jorge Bruned Alamán <bruned.127631@e.unavarra.es>
2022-02-26 01:42:04 +0100

    Implement basic backend with Flask

--- easips/gui.py
+++ easips/gui.py
@@ -7,3 +7,18 @@
 # Flask should also deliver static HTML files (login, dashboard and service)
 
 # Flask should also have POST endpoints to receive AJAX requests (unblock IPs, create services...)
+
+from flask import Flask, send_file
+app = Flask(__name__)
+  
+@app.route('/')
+def dashboard():
+   return send_file("web/dashboard.html")
+   
+@app.route('/service')
+def service():
+   return send_file("web/service.html")
+   
+@app.route('/assets/<filename>')
+def asset(filename):
+   return send_file(f"web/assets/{filename}")
