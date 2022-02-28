17211ba812a8d23d18ba10557639d5864bfb32b6
Thomas Werthenbach <t.a.k.werthenbach@student.tudelft.nl>
2022-02-28 19:45:46 +0100

    added ssh regex

--- log-sniffer/main.py
+++ log-sniffer/main.py
@@ -1,8 +1,14 @@
 from logsniffer import *
 
 joomla_log_location = "../joomla/webserver/administrator/logs/error.php"
-joomla_log_testcontainer = "test.txt"
 # language=regexp
-sniffer = LogSniffer(joomla_log_location, r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}\tINFO\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\tjoomlafailure\t.*Username.*password.*not.*match.*')
+joomla_log_regex = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}\tINFO\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\tjoomlafailure\t.*Username.*password.*not.*match.*'
+
+ssh_log_location = "../ssh/logs/auth.log"
+# language=regexp
+ssh_log_regex = r'^\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\ssshserver.*authentication\sfailure.*rhost=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*'
+
+
+sniffer = LogSniffer(ssh_log_location, ssh_log_regex)
 sniffer.start()
 
