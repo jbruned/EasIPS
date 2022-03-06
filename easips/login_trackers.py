7ad8acb6ec72fc0d5b6405774c91c9bdb3b0bf2a
Jorge Bruned Alamán <bruned.127631@e.unavarra.es>
2022-03-06 13:27:08 +0100

    Modified LogSniffer to use a poll strategy

--- easips/login_trackers.py
+++ easips/login_trackers.py
@@ -1,27 +1,39 @@
 import time
-import os
-import re
+from os.path import getsize
+from re import compile
+from abc import ABC, abstractmethod
+
+
+class LoginTracker (ABC):
+
+    @abstractmethod
+    def poll(self):
+        """
+        This method should return a list with all the IP addresses that performed
+        a failed login attempt since the last call to this method.
+        If an IP address has failed X times, it should appear X times in the list.
+        """
+        raise NotImplementedError
 
 class LogSniffer:
-    file = None
-    def __init__(self, log_location: str, log_regex: str):
+    """
+    Detects failed login attempts by reading the log files and applying a regular expression
+    """
 
-        self.log_location = log_location
-        self.pattern = re.compile(log_regex)
+    def __init__(self, log_path: str, log_regex: str):
+        self.log_path = log_path
+        self.previous_size = getsize(self.log_location)
+        self.log_file = open(self.log_path, 'r')
+        self.log_file.read()
+        self.pattern = compile(log_regex)
 
-    def start(self):
-        self.file = open(self.log_location, 'r')
-        self.file.read()
-
-        previous_size = os.path.getsize(self.log_location)
-        while True:
-            if previous_size > (size := os.path.getsize(self.log_location)):
-                self.file.close()
-                self.file = open(self.log_location, 'r')
-                self.file.read()
-                previous_size = size
-                print("File emptied!")
-
-            if (line := self.file.readline().strip()) and (match := self.pattern.match(line)):
-                print('failed login from ', match.groups()[0])
-            time.sleep(0.001)
+    def poll(self) -> list:
+        if self.previous_size > (size := os.path.getsize(self.log_location)):
+            self.file.close()
+            self.file = open(self.log_path, 'r')
+            self.file.read()
+            previous_size = size
+        # TODO: read all the new lines, maybe there are several new logs
+        if (line := self.file.readline().strip()) and (match := self.pattern.match(line)):
+            ip = match.groups()[0]
+        return [ip]
