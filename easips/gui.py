24ab10fa46f977b43aae410f128bddd2abb93416
Jorge Bruned Alamán <bruned.127631@e.unavarra.es>
2022-03-07 18:16:55 +0100

    Removed already completed TODO

--- easips/gui.py
+++ easips/gui.py
@@ -126,11 +126,3 @@ class WebGUI:
                 return send_file(f"web/assets/{filename}")
             except FileNotFoundError:
                 abort(404)
-
-        # TODO: implement API using Flask with two GET endpoints:
-        # - /services/ -> Return a list of services in JSON (fields: service id, name,
-        #                 blocked IPs, both currently and past 24h, minutes since last block)
-        # - /services/{id} -> Return a list of blocked IPs in JSON (fields: address,
-        #                     minutes since block started)
-
-        # Flask should also have POST endpoints to receive AJAX requests (unblock IPs, create services...)
